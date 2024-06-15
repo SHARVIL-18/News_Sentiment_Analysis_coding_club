42afae7198d4b0eb7346d2354f3336e7be877905
Shivam Savani <shivamsavani935@gmail.com>
2024-06-15 13:06:59 +0530

    bug fixes

--- core/views.py
+++ core/views.py
@@ -34,17 +34,15 @@ def home(request):
             messages.success(request, "You Have Been Logged In!")
             return redirect('home')
         else:
-            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            messages.error(request, "There Was An Error Logging In, Please Try Again...")
             return redirect('home')
 
     else:
-
         query = request.GET.get('query')
         results = []
         if query:
             query = query.replace(" ", "+")
             link = f"https://news.google.com/search?q={query}&hl=en-IN&gl=IN&ceid=IN:en"
-            # link = f"https://www.google.com/search?lr=lang_en&q={query}&sca_esv=7b1f93cccdc9738a&sca_upv=1&rlz=1C1UEAD_enIN1066IN1066&biw=1536&bih=398&tbm=nws&sxsrf=ADLYWILa2YTPLYepGsgLG385nFveSa_kmQ%3A1716828649665&ei=6blUZoWfKM2gseMP7qqZgAc&ved=0ahUKEwjFt7aOpa6GAxVNUGwGHW5VBnAQ4dUDCA0&uact=5&oq=bjp&gs_lp=Egxnd3Mtd2l6LW5ld3MiA2JqcDIREAAYgAQYkQIYsQMYgwEYigUyCxAAGIAEGLEDGIMBMggQABiABBixAzIKEAAYgAQYQxiKBTIKEAAYgAQYQxiKBTILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgoQABiABBhDGIoFMggQABiABBixAzIIEAAYgAQYsQNI8YgBUNOAAVj1hQFwAHgAkAEAmAH1AaAB3wWqAQUwLjIuMrgBA8gBAPgBAZgCBKAChgbCAgQQABgDwgIOEAAYgAQYkQIYsQMYigXCAg4QABiABBixAxiDARiKBcICEBAAGIAEGLEDGEMYgwEYigWYAwCIBgGSBwUwLjIuMqAHuhU&sclient=gws-wiz-news"
             req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
             webpage = urlopen(req).read()
             soup = BeautifulSoup(webpage, 'html5lib')
@@ -53,14 +51,9 @@ def home(request):
             linkset = set()
             for link in soup.find_all('a'):
                 link_str = str(link.get('href'))
-                # link_split = (link_str.split("/url?q="))
-                # if len(link_split) == 2:
-                #     link_str = link_split[1].split('&sa=U&')[0]
-                if len(linkset) == 10:
+                if len(linkset) >= 10:
                     break
                 try:
-                    # if link_str.startswith(
-                    #         "https://") and 'google.com' not in link_str and 'youtube.com' not in link_str and 'blogger.com' not in link_str:
                     if link_str.startswith("./articles"):
                         if link_str in linkset:
                             continue
@@ -82,46 +75,37 @@ def home(request):
                     print(f"Error processing article at {link_str}: {e}")
                     continue
 
-            df = pd.DataFrame(articles)
+            if articles:
+                df = pd.DataFrame(articles)
 
-            def preprocess_text(text):
-                tokens = word_tokenize(text.lower())
-                filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
-                lemmatizer = WordNetLemmatizer()
-                lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
-                processed_text = ' '.join(lemmatized_tokens)
-                return processed_text
+                def preprocess_text(text):
+                    tokens = word_tokenize(text.lower())
+                    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+                    lemmatizer = WordNetLemmatizer()
+                    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+                    processed_text = ' '.join(lemmatized_tokens)
+                    return processed_text
 
-            analyzer = SentimentIntensityAnalyzer()
+                analyzer = SentimentIntensityAnalyzer()
 
-            def get_sentiment(text):
+                def get_sentiment(text):
+                    scores = analyzer.polarity_scores(text)
+                    compound = scores['compound']
+                    return compound
 
-                scores = analyzer.polarity_scores(text)
-                compound = scores['compound']
-                return compound
+                df['processed_title'] = df['title'].apply(preprocess_text)
+                df['processed_text'] = df['text'].apply(preprocess_text)
 
-            df['processed_title'] = df['title'].apply(preprocess_text)
-            df['processed_text'] = df['text'].apply(preprocess_text)
+                df['sentiment_score_title'] = df['processed_title'].apply(get_sentiment)
+                df['sentiment_score_text'] = df['processed_text'].apply(get_sentiment)
 
-            df['sentiment_score_title'] = df['processed_title'].apply(get_sentiment)
-            df['sentiment_score_text'] = df['processed_text'].apply(get_sentiment)
+                df['average_sentiment_score'] = (df['sentiment_score_title'] + df['sentiment_score_text']) / 2
 
-            df['average_sentiment_score'] = (df['sentiment_score_title'] + df['sentiment_score_text']) / 2
-            # In your view, update the code to use the new get_sentiment function
-            #        df['sentiment_title'], df['sentiment_score'] = zip(*df['processed_title'].apply(get_sentiment))
-            #       df['sentiment_text'], _ = zip(*df['text'].apply(get_sentiment))
-
-            #     df['sentiment_title'], df['sentiment_score_title'] = zip(*df['processed_title'].apply(get_sentiment))
-            #    # df['sentiment_text'], df['sentiment_score_text'] = zip(*df['text'].apply(get_sentiment))
-            #     _, df['sentiment_score_text'] = zip(*df['text'].apply(get_sentiment))
-
-            #   df['sentiment_title'] = df['processed_title'].apply(get_sentiment)
-            #    df['sentiment_text'] = df['text'].apply(get_sentiment)
-
-            # Convert DataFrame to list of dictionaries
-            results = df.to_dict('records')
-            print(results)
-        # Pass the data to the template
+                results = df.to_dict('records')
+                print(results)
+            else:
+                print("No articles found.")
+                messages.warning(request, "No articles found for the query.")
         return render(request, 'core/home.html', {'results': results})
 
 
@@ -150,8 +134,6 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'core/register.html', {'form': form})
-
     return render(request, 'core/register.html', {'form': form})
 
 
